95c27fb09fbf1dfb074d19f7c4df2a6781856c2e
Nehal Nevle <nehalsuperdude@gmail.com>
2020-10-24 16:53:50 +0530

    intial commit

---
+++ two_wheeled/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nehal/hacktober/two_wheeled/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/nehal/hacktober/two_wheeled/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nehal/hacktober/two_wheeled/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/nehal/hacktober/two_wheeled/src:$ROS_PACKAGE_PATH"
